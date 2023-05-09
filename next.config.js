10d0f8983bf3147679c79279f1935770f9d8efb1
Blake Ingenthron <blakeingenthron@gmail.com>
2023-05-09 18:01:41 -0500

    testing builder

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  typescript: {
+    // Set this to false if you want production builds to abort if there's type errors
+    ignoreBuildErrors: process.env.VERCEL_ENV === "production",
+  },
+  eslint: {
+    /// Set this to false if you want production builds to abort if there's lint errors
+    ignoreDuringBuilds: process.env.VERCEL_ENV === "production",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
