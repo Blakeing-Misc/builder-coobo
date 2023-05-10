7c1f115fffedbf40695ec8b8c50333632b6e9b49
Blake Ingenthron <blakeingenthron@gmail.com>
2023-05-10 13:22:56 -0500

    add blog

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { hostname: "cdn.builder.io" },
+      { hostname: "source.unsplash.com" },
+      {
+        hostname: "images.unsplash.com",
+      },
+      {
+        hostname: "tailwindui.com",
+      },
+    ],
+  },
   typescript: {
     // Set this to false if you want production builds to abort if there's type errors
     ignoreBuildErrors: process.env.VERCEL_ENV === "production",
---
+++ src/pages/blog/[handle].tsx
@@ -0,0 +1,91 @@
+// pages/blog/[handle].jsx
+import {
+  builder,
+  BuilderComponent,
+  BuilderContent,
+  useIsPreviewing,
+} from "@builder.io/react";
+import Head from "next/head";
+import DefaultErrorPage from "next/error";
+import React from "react";
+import Image from "next/image";
+
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY || "");
+
+function BlogArticle({ article }) {
+  const isPreviewing = useIsPreviewing();
+  if (!article && !isPreviewing) {
+    return (
+      <>
+        <Head>
+          <meta name="robots" content="noindex" />
+        </Head>
+        <DefaultErrorPage statusCode={404} />
+      </>
+    );
+  }
+
+  return (
+    <BuilderContent
+      content={article}
+      options={{ includeRefs: true }}
+      model="blog-article"
+    >
+      {(data, loading, fullContent) => (
+        <React.Fragment>
+          <Head>
+            {/* Render meta tags from custom field */}
+            <title>{data?.title}</title>
+            <meta name="description" content={data?.blurb} />
+            <meta name="og:image" content={data?.image} />
+          </Head>
+
+          <div>
+            <div>{data?.title}</div>
+            <Image
+              src={data?.image}
+              alt={data?.title}
+              width={600}
+              height={400}
+            />
+            {/* Render the Builder drag/drop'd content */}
+            <BuilderComponent
+              model="blog-article"
+              content={fullContent}
+              options={{ includeRefs: true }}
+            />
+          </div>
+        </React.Fragment>
+      )}
+    </BuilderContent>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const article =
+    (await builder
+      .get("blog-article", {
+        // Include references, like our `author` ref
+        options: { includeRefs: true },
+        query: {
+          // Get the specific article by handle
+          "data.handle": params.handle,
+        },
+      })
+      .promise()) || null;
+
+  return {
+    props: {
+      article,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true,
+  };
+}
+
+export default BlogArticle;
